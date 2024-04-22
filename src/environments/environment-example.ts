8ef092b554a453853c4b4ccbdb1d102d9a254229
Harlynton <harlynton.castano@gmail.com>
2024-04-22 15:08:47 -0500

    prueba

--- src/environments/environment-example.ts
+++ src/environments/environment-example.ts
@@ -1,10 +1,10 @@
 export const environment= {
     firebase: {
-        apiKey: "",
-        authDomain: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: "2"
+        apiKey: "AIzaSyBQPQ3qnsLuBj5ukz3Pl2X02zVspF9DOpw",
+        authDomain: "miprimerapp-3a922.firebaseapp.com",
+        projectId: "miprimerapp-3a922",
+        storageBucket: "miprimerapp-3a922.appspot.com",
+        messagingSenderId: "147842953167",
+        appId: "1:147842953167:web:cb45bbdf23c33478632cba"
     },
 }
